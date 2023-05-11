6af4598d42f55b33c9a55644286400b44b84198e
Merul Panwala <mmp@narola.email>
2023-05-11 11:11:56 +0530

    final commit

--- src/forms/mainForm.jsx
+++ src/forms/mainForm.jsx
@@ -5,6 +5,7 @@ import BasicFormInfo from './basicInfoForm'
 import AccountForm from './accountForm'
 import AddressForm from './addressForm'
 import EducationForm from './educationForm'
+import FinalOutput from './finalOutput'
 
 const MainForm = () => {
     const methods = useForm()
@@ -32,10 +33,12 @@ const MainForm = () => {
     const [data, setData] = useState(initialState)
 
     const submitHandler = (values) => {
+        console.log("This runs...");
         if (page === formTitles.length - 1) {
             console.log('data', data)
             setData({ ...data, ...values })
             localStorage.setItem("Data", JSON.stringify(data))
+            setPage((currPage) => currPage + 1);
         } else {
             setData({ ...data, ...values })
             setPage((currPage) => currPage + 1);
@@ -51,15 +54,24 @@ const MainForm = () => {
             return <AddressForm formTitle={formTitles[2]} data={data} setData={setData} />
         } else if (page === 3) {
             return <EducationForm formTitle={formTitles[3]} data={data} setData={setData} page={page} setPage={setPage} />
+        } else {
+            return <FinalOutput data={data} setData={setData} formTitles={formTitles} />
         }
     };
+
+    // const handleSubmitWithoutPropagation = (e) => {
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    //     console.log('e', e)
+    //     methods.handleSubmit(submitHandler)(e);
+    // };
 
     return (
         <Container className='border py-4 w-75'>
             <FormProvider {...methods}>
                 <Form onSubmit={methods.handleSubmit(submitHandler)}>
                     {PageDisplay(page)}
-                    <div className="mt-3 text-center">
+                    {page < 4 && <div className="mt-3 text-center">
                         <Button
                             className='mx-3'
                             variant='outline-secondary'
@@ -75,7 +87,7 @@ const MainForm = () => {
                             variant='outline-primary'>
                             {page === formTitles.length - 1 ? "Submit" : "Next"}
                         </Button>
-                    </div>
+                    </div>}
                 </Form>
             </FormProvider>
         </Container>
